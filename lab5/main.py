d4d408799d2ecd34c1d6f55db5ad1af24c173004
Bloodielie <riopro2812@gmail.com>
2021-09-06 11:38:44 +0300

    refactoring: global refactoring

--- lab5/main.py
+++ lab5/main.py
@@ -1,8 +1,9 @@
 import os
-from typing import List, Callable
+from typing import List, Callable, Dict
 from pprint import pprint
 
 Students = List[List[str]]
+MenuFunc = Callable[[Students], None]
 
 
 def read_data(path: str) -> str:
@@ -37,9 +38,21 @@ def bad_input(_: Students) -> None:
     return print("Bad menu number")
 
 
-def get_func(num: str) -> Callable[[Students], None]:
-    func_mapping = {"1": increase_age, "2": save_students_to_file, "3": receive_student_from_file}
-    return func_mapping.get(num, bad_input)
+def get_menu_func(func_mapping: Dict[str, MenuFunc]) -> Callable[[str], MenuFunc]:
+    def get_menu_func_inner(num: str) -> MenuFunc:
+        return func_mapping.get(num, bad_input)
+
+    return get_menu_func_inner
+
+
+def menu(students: Students, menu_text: str, get_func_callback: Callable[[str], MenuFunc]) -> None:
+    while True:
+        menu_number = input(menu_text)
+        if menu_number == "0":
+            break
+
+        get_func_callback(menu_number)(students)
+        pprint(students)
 
 
 def task1() -> None:
@@ -55,39 +68,31 @@ def task2() -> None:
 def task3() -> None:
     print("Task 3:")
     students = convert_data_to_csv(read_data("students.csv"))
-    while True:
-        menu_number = input("Menu:\n0 - Exit\n1 - Increase age by group: \n")
-        if menu_number == "0":
-            break
-
-        get_func(menu_number)(students)
-        pprint(students)
+    return menu(
+        students,
+        "Menu:\n0 - Exit\n1 - Increase age by group: \n",
+        get_menu_func({"1": increase_age})
+    )
 
 
 def task4() -> None:
     print("Task 4:")
     students = convert_data_to_csv(read_data("students.csv"))
-    while True:
-        menu_number = input("Menu:\n0 - Exit\n1 - Increase age by group \n2 - Save students to file \n")
-        if menu_number == "0":
-            break
-
-        get_func(menu_number)(students)
-        pprint(students)
+    return menu(
+        students,
+        "Menu:\n0 - Exit\n1 - Increase age by group \n2 - Save students to file \n",
+        get_menu_func({"1": increase_age, "2": save_students_to_file})
+    )
 
 
 def task5() -> None:
     print("Task 5:")
     students = convert_data_to_csv(read_data("students.csv"))
-    while True:
-        menu_number = input(
-            "Menu:\n0 - Exit\n1 - Increase age by group \n2 - Save students to file \n3 - Get data from file\n"
-        )
-        if menu_number == "0":
-            break
-
-        get_func(menu_number)(students)
-        pprint(students)
+    return menu(
+        students,
+        "Menu:\n0 - Exit\n1 - Increase age by group \n2 - Save students to file \n3 - Get data from file\n",
+        get_menu_func({"1": increase_age, "2": save_students_to_file, "3": receive_student_from_file})
+    )
 
 
 def main() -> None:
